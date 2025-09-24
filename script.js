36e0600c4ffe7244e8ddeffa711d1f96491f3966
amirmohammad <amirm.1774@gmail.com>
2025-09-25 02:42:35 +0330

    editing *edit action*

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
 
+const editAction_block = () => {
+  const lists = document.querySelectorAll("li");
+
+  lists.forEach((item) => {
+    item.classList.add("btn-block");
+  });
+};
+const editAction_unBlock = () => {
+  const lists = document.querySelectorAll("li");
+
+  lists.forEach((item) => {
+    item.classList.remove("btn-block");
+  });
+};
+
 // Add Task Event
 addTaskBtn.addEventListener("click", () => {
   const taskText = taskInput.value.trim();
@@ -14,6 +29,7 @@ addTaskBtn.addEventListener("click", () => {
     // Task text
     const input = document.createElement("input");
     input.setAttribute("disabled", "true");
+    input.setAttribute("placeholder", "You should type a task name...");
     input.value = taskText;
 
     // Group button
@@ -40,16 +56,30 @@ addTaskBtn.addEventListener("click", () => {
 
     // Edit action
     i.addEventListener("click", () => {
+      editAction_block();
+      li.classList.remove("btn-block");
       i.classList.add("btn-disable");
       deleteBtn.classList.add("btn-disable");
       edit_done_btn.classList.remove("btn-disable");
       input.removeAttribute("disabled");
+      input.focus();
+      input.select();
+      addTaskBtn.classList.add("btn-block");
+      clearList.classList.add("btn-block");
+      taskInput.classList.add("btn-block");
+      taskInput.value = "";
     });
     edit_done_btn.addEventListener("click", () => {
-      i.classList.remove("btn-disable");
-      deleteBtn.classList.remove("btn-disable");
-      edit_done_btn.classList.add("btn-disable");
-      input.setAttribute("disabled", "true");
+      if (input.value !== "") {
+        i.classList.remove("btn-disable");
+        deleteBtn.classList.remove("btn-disable");
+        edit_done_btn.classList.add("btn-disable");
+        input.setAttribute("disabled", "true");
+        taskInput.classList.remove("btn-block");
+        addTaskBtn.classList.remove("btn-block");
+        clearList.classList.remove("btn-block");
+        editAction_unBlock();
+      }
     });
     // Delete action (basic)
     deleteBtn.addEventListener("click", () => {
